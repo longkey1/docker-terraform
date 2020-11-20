32a478b06d2786732ed13a7e495c1ebc9e11e7ee
longkey1 <longkey1@gmail.com>
2020-11-20 13:45:48 +0900

    fix: entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
 USER_ID=${LOCAL_UID:-9001}
 GROUP_ID=${LOCAL_GID:-9001}
 
 echo "Starting with UID : ${USER_ID}, GID: ${GROUP_ID}"
-useradd -u ${USER_ID} -o -d ${WORK_DIR} worker
+adduser -u ${USER_ID} -h ${WORK_DIR} --disabled-password worker
 groupmod -g ${GROUP_ID} worker
 export HOME=/work
 
 chown worker:worker ${WORK_DIR}
 
-exec /usr/sbin/gosu worker "$@"
+exec /sbin/su-exec worker "$@"
